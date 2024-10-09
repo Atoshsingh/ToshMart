2be9eeb826a53cb78b34f97fafbcb78cb9c06da7
Atoshsingh <toshkumrt@gmail.com>
2024-10-09 07:50:11 +0530

    udpated the content of folders

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "https://toshmart.onrender.com",
-      "/uploads/": "https://toshmart.onrender.com",
+      "/api/": "http://localhost:5000",
+      "/uploads/": "http://localhost:5000",
     }
   }
 })
